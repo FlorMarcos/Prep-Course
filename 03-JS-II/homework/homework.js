840bcb9d02ea157753921db5cdde114dbb26651d
Your Name <florcita.17@live.com.ar>
2022-08-01 20:44:29 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,8 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+
+  String = string
 }
 
 function mayoriaDeEdad(edad) {
